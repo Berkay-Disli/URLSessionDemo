f4d76551b1caae9114526fa4169ce4f69e23e5c2
Berkay-Disli <berkaydisli.work@gmail.com>
2022-05-05 13:49:53 +0300

    Initial Commit

---
+++ ExcusesAPI/ExcusesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExcusesAPIApp.swift
+//  ExcusesAPI
+//
+//  Created by Berkay Disli on 5.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExcusesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
